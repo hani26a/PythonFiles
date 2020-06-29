f72af8eeb714ef473966af195e1d4595deb04efc
hani26a <42440028+hani26a@users.noreply.github.com>
2020-06-29 09:55:17 +0530

    Add files via upload

---
+++ hello (1).py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+s1 = "Hello"
+print(s1)
+
+
+# In[3]:
+
+
+s2 = 'Hello'
+print(s2)
+
+
+# In[4]:
+
+
+s3 = '"Hello"'
+print(s3)
+
+
+# In[5]:
+
+
+#this is a comment
+
+
+# In[8]:
+
+
+#how to declare and use variables
+x = 5
+print(x)
+
+
+# In[9]:
+
+
+#introduction to datatypes
+#String == str
+#list == list
+#tuple == tuple 
+#dictionary == dict
+
+#to verify and validate data types
+
+type(s3)
+
+
+# In[10]:
+
+
+#Enhancement of data types
+print(s3.Title)
+
+
+# In[11]:
+
+
+print(s3.title)
+
+
+# In[12]:
+
+
+print(s3.title())
+
+
+# In[13]:
+
+
+#Day3
+#Introduction to fstrings
+
+
+# In[14]:
+
+
+#general syntax to fstrings
+
+
+# In[15]:
+
+
+#f"{place holder}{placeholder"}
+
+
+# In[16]:
+
+
+first_name = "Syeda"
+
+
+# In[17]:
+
+
+last_name = "Hani"
+
+
+# In[18]:
+
+
+full_name = f"{first_name}{last_name}""
+
+
+# In[19]:
+
+
+#adding white spaces to strings
+
+
+# In[20]:
+
+
+print(' python')
+
+
+# In[21]:
+
+
+#adding tab dilimeter
+
+
+# In[22]:
+
+
+print('\t python')
+
+
+# In[23]:
+
+
+print("fav_ languages: \n python \n c++ \n java")
+
+
+# In[24]:
+
+
+print("fav_languages: \n\t python \n\t c++ \n\t java")
+
+
+# In[25]:
+
+
+#Removing white spaces
+
+
+# In[26]:
+
+
+fav_lang2 = "java" 
+
+
+# In[27]:
+
+
+fav_lang3 = "  java"
+
+
+# In[28]:
+
+
+print(fav_lang3.lstrip())
+
+
+# In[29]:
+
+
+fav_lang4 = "ruby  "
+
+
+# In[30]:
+
+
+print(fav_lang4.rstrip())
+
+
+# In[ ]:
+
+
+
+
